87f9ac5d8192b033f432b2f30c2110cf88ca3e4a
Pratham <prathamjaiswal204@gmail.com>
2026-07-06 22:01:47 +0530

    fix: wrap Hono app with /api prefix using hono/node-server

--- api/[...route].ts
+++ api/[...route].ts
@@ -1,6 +1,10 @@
 import { handle } from 'hono/node-server/vercel'
+import { Hono } from 'hono'
 import { app as apiApp } from './_bundle.mjs'
 
 export const config = { runtime: 'nodejs' }
 
-export default handle(apiApp)
+const wrapper = new Hono()
+wrapper.route('/api', apiApp)
+
+export default handle(wrapper)
